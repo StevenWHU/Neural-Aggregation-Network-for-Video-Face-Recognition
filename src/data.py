f314241f3f63444705f7baa48ed84e0bef01c49d
StevenWHU <120295449@qq.com>
2018-07-05 21:32:12 +0800

    fix

--- src/data.py
+++ src/data.py
@@ -66,7 +66,6 @@ class Data(object):
 
 if __name__ == "__main__":
     filename = "./YoutubeFaces.mat"
-    dataset = Data(filename, 3, 1595) 
     dataset.load_feature()
     train_features, train_label = dataset.next_batch(5)
     print train_features, train_label
